c49a0a665941ba4a39f73952f8c934d063a3cd69
Chad Smith <smith.chad12@gmail.com>
2020-03-11 19:45:32 -0500

    example of how to captureStream

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,19 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="6">
-        <v-select
-          label="Video Device"
-          :items="videoDevices"
-          item-value="deviceId"
-          item-text="label"
-          v-model="currentVideoDevice">
-        </v-select>
-      </v-col>
-      <v-col cols="6">
-        <v-select
-          label="Audio Output Device"
-          :items="audioOutputDevices"
-          item-value="deviceId"
-          item-text="label"
-          v-model="currentAudioOutputDevice"
-          @change="updateSinkId">
-        </v-select>
-      </v-col>
-      <v-col class="mb-5" cols="12">
-        <v-btn @click="accessCamera">{{ recordingButtonText }}</v-btn>
-      </v-col>
-    </v-row>
-    <v-row class="text-center">
       <v-col cols="12">
-        <video id="localVideo" controls></video>
+        <v-btn @click="onClickStream">
+          {{ recordingButtonText }}
+        </v-btn>
+      </v-col>
+      <v-col cols="6">
+        <video id="localVideo" autoplay controls muted loop playsinline>
+          <source :src="require('../assets/videos/chrome.mp4')"/>
+          <source :src="require('../assets/videos/chrome.webm')"/>
+        </video>
+      </v-col>
+      <v-col cols="6">
+        <video id="videoStream" playsinline autoplay></video>
       </v-col>
     </v-row>
   </v-container>
@@ -34,99 +21,51 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-import { getDevices, getLocalTracks } from "@/lib/media-devices";
-import {
-  DeviceSelectionOptions
-} from "../lib/DeviceSelectionOptions";
-import { LocalMediaTrack } from "@/lib/tracks/index";
-import { Nullable, getValue, hasValue } from "@/lib/Nullable";
+import { Nullable, getValue } from '@/lib/Nullable';
 
 @Component
 export default class HelloWorld extends Vue {
-  devices: Nullable<DeviceSelectionOptions> = null;
-  currentVideoDevice = "";
-  currentAudioOutputDevice = "";
-  showVideo = false;
+  isStreaming = false;
   localVideo: Nullable<HTMLVideoElement> = null;
-  recordedBlobs: Blob[] = [];
-  localTracks: LocalMediaTrack[] = [];
+  videoStream: Nullable<HTMLVideoElement> = null;
+  stream: Nullable<MediaStream> = null;
 
   get recordingButtonText()  {
-    return this.showVideo ? 'Stop Recording' : 'Start Recording';
+    return !this.isStreaming ? 'Start Streaming' : 'Stop Streaming';
   }
 
-  get videoDevices() {
-    if(!hasValue(this.devices)) {
-      return [];
-    }
-
-    return getValue(this.devices).deviceGroups.videoinput;
-  }
-
-  get audioOutputDevices() {
-    if(!hasValue(this.devices)) {
-      return [];
-    }
-
-    return getValue(this.devices).deviceGroups.audiooutput;
-  }
-
-  updateSinkId() {
-    // we have to use any here as typescript doesn't know about setSinkId yet
-    // eslint-disable-next-line
-    const videoElement = getValue(this.localVideo) as any;
-    if(typeof videoElement.sinkId !== 'undefined') {
-      videoElement.setSinkId(this.currentAudioOutputDevice)
-        .catch(() => {
-          // reset to the first index something happened
-          this.currentAudioOutputDevice = 'default';
-        });
-    }
-  }
-
-  async getTracks() {
-    const constraints: MediaStreamConstraints = {
-      audio: {
-        echoCancellation: true,
-        deviceId: this.currentAudioOutputDevice
-      },
-      video: {
-        deviceId: this.currentVideoDevice
-      }
-    };
-    const tracks = await getLocalTracks(constraints);
-    const mediaStream = new MediaStream(tracks.map(track => track.mediaStreamTrack));
-
-    this.showVideo = true;
-    this.localTracks = tracks;
-    getValue(this.localVideo).srcObject = mediaStream;
-  }
-
-  stopTracks() {
-    this.showVideo = false;
-    getValue(this.localVideo).srcObject = null;
-    for(const track of this.localTracks) {
-      track.stop();
-    }
-
-    this.localTracks = [];
-  }
-
-  async accessCamera() {
-    if(!this.showVideo) {
-      await this.getTracks();
+  onClickStream() {
+    if(!this.isStreaming) {
+      this.startStream();
     }
     else {
-      this.stopTracks();
+      this.stopStream();
     }
   }
 
-  async mounted() {
-    this.devices = await getDevices();
-    this.localVideo = document.querySelector('#localVideo');
+  startStream() {
+    // eslint-disable-next-line
+    const local = getValue(this.localVideo) as any;
+    if(local.captureStream) {
+      this.stream = local.captureStream();
+    }
+    else if(local.mozCaptureStream) {
+      this.stream = local.mozCaptureStream();
+    }
 
-    this.currentVideoDevice = this.devices.deviceGroups.videoinput[0].deviceId;
-    this.currentAudioOutputDevice = this.devices.deviceGroups.audiooutput[0].deviceId;
+    getValue(this.videoStream).srcObject = this.stream;
+    this.isStreaming = true;
+  }
+
+  stopStream() {
+    this.stream = null;
+    this.isStreaming = false;
+    getValue(this.videoStream).srcObject = null;
+  }
+
+  mounted() {
+    this.localVideo = document.querySelector('#localVideo');
+    this.videoStream = document.querySelector('#videoStream');
   }
 }
 </script>
